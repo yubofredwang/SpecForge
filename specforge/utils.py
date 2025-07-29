ec271504d40175214d28169e987d6dbd0e427366
Yubo Wang <yubowang2019@gmail.com>
2025-07-29 08:04:03 +0000

    profiler

--- specforge/utils.py
+++ specforge/utils.py
@@ -2,10 +2,11 @@ import json
 import os
 import re
 from contextlib import contextmanager
-from datetime import timedelta
+from datetime import datetime
 
 import torch
 import torch.distributed as dist
+from torch.profiler import schedule
 from transformers import PretrainedConfig
 
 
@@ -68,3 +69,24 @@ def get_last_checkpoint(folder):
         folder,
         max(checkpoints, key=lambda x: int(_re_checkpoint.search(x).groups()[0])),
     )
+
+
+def profiler_schedule(wait=1, warmup=2, active=2, repeat=1):
+    return schedule(
+        wait=wait,
+        warmup=warmup,
+        active=active,
+        repeat=repeat,
+    )
+
+def export_profiler_trace(profiler):
+    timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
+    trace_name = f"profiler_rank{dist.get_rank()}_{timestamp}.pt.trace.json"
+    trace_path = os.path.join(os.environ["SGLANG_TORCH_PROFILER_DIR"], trace_name)
+    profiler.export_chrome_trace(trace_path)
+    print(f"Profiler data saved to {trace_path}")
+    memory_timeline_name = f"profiler_rank{dist.get_rank()}_{timestamp}.html"
+    memory_timeline_path = os.path.join(os.environ["SGLANG_TORCH_PROFILER_DIR"], memory_timeline_name)
+    profiler.export_memory_timeline(memory_timeline_path)
+    print(f"Memory timeline data saved to {memory_timeline_path}")
+    print(f"Stopped PyTorch profiler")
