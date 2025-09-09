e8fd39a70e9c5f6a7927d67c638e19f9367d3b8a
jiapingW <56055330+jiapingW@users.noreply.github.com>
2025-09-09 17:22:33 +0800

    Fixed the bug that caused the training to stop directly when the training data was not alternating between user and assistant. (#221)
    * fix bug
    
    * Update parse.py

--- specforge/data/parse.py
+++ specforge/data/parse.py
@@ -68,7 +68,11 @@ class GeneralParser(Parser):
             convroles = ["user", "assistant"]
             for j, sentence in enumerate(conversation):
                 role = sentence["role"]
-                assert role == convroles[j % 2], f"unexpected role {role}"
+                if role != convroles[j % 2]:
+                    warnings.warn(
+                        f"Conversation truncated due to unexpected role '{role}'. Expected '{convroles[j % 2]}'."
+                    )
+                    break
                 messages.append({"role": role, "content": sentence["content"]})
 
             conversation = self.tokenizer.apply_chat_template(
