fix: remove attention mask shift in flex attention implementation (#245)
* fix attn mask in flex_attention imp

* remove the attn mask shift in sdpa imp

* fix the attn shift in ut

* fix the print_with_rank when process group not initialized

* fix lint

---------

Co-authored-by: root <root@g0585.prod.pyl1.voltagepark.net>

tests/test_utils/test_flex_attention.py
@@ -134,14 +134,7 @@ class TestFlexAttention(unittest.TestCase):
                 atol=1e-2,
                 rtol=1e-2,
             )
-            if not is_last:
-                # Step 5.7: we need to update the loss mask
-                ind = torch.arange(seq_len, device=decoder_attention_mask.device)
-                ind0 = ind[idx:]
-                ind1 = ind[: seq_len - idx]
-                decoder_attention_mask[:, :, ind0, ind1] = torch.finfo(
-                    decoder_attention_mask.dtype
-                ).min
+
             # Check output shape
             expected_output_shape = (batch_size, seq_len, self.config.hidden_size)
             self.assertEqual(output_flex.shape, expected_output_shape)
@@ -238,12 +231,6 @@ class TestFlexAttention(unittest.TestCase):
 
             if not is_last:
                 # Step 5.7: we need to update the loss mask
-                ind = torch.arange(seq_len, device=decoder_attention_mask.device)
-                ind0 = ind[idx:]
-                ind1 = ind[: seq_len - idx]
-                decoder_attention_mask[:, :, ind0, ind1] = torch.finfo(
-                    decoder_attention_mask.dtype
-                ).min
                 loss_mask = padding(loss_mask, left=False)
         mean_loss = sum(loss_list) / len(loss_list)
         mean_loss_flex = sum(loss_flex_list) / len(loss_flex_list)
@@ -268,14 +255,16 @@ class TestEagle3FlexMask(unittest.TestCase):
         D = 128
         Q_LEN = S
         KV_LEN = S * 3
+        lck = 128 * 2
         data_type = torch.bfloat16
         query = norm_tensor((B, H, S, D), device="cuda", dtype=data_type)
         key_cache = norm_tensor((B, H, KV_LEN, D), device="cuda", dtype=data_type)
         value_cache = norm_tensor((B, H, KV_LEN, D), device="cuda", dtype=data_type)
         seq_lengths = torch.tensor([S], device="cuda", dtype=torch.int32)
+        seq_lengths -= lck
         block_mask = compile_friendly_create_block_mask(
             mask_mod=generate_eagle3_mask(
-                seq_lengths=seq_lengths, Q_LEN=Q_LEN, KV_LEN=KV_LEN, shift_left=128 * 2
+                seq_lengths=seq_lengths, Q_LEN=Q_LEN, KV_LEN=KV_LEN, lck=lck
             ),
             B=1,
             H=1,
@@ -285,14 +274,14 @@ class TestEagle3FlexMask(unittest.TestCase):
         )
         # fmt: off
         expected_mask = torch.tensor([[[
-            [0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0],
-            [0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0],
-            [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0],
-            [1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0],
-            [1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0],
-            [1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0],
-            [1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0],
-            [1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1],
+            [1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0],
+            [1, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0],
+            [1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0],
+            [1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0],
+            [1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0],
+            [1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
         ]]], dtype=torch.int32).to(query.device)
         # fmt: on
         dense_mask = block_mask.to_dense()