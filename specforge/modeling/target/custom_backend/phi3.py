53db109812f0f52c332f13787ea347182f661c69
Yubo Wang <yubowang2019@gmail.com>
2025-11-29 00:38:45 +0000

    fix phi

--- specforge/modeling/target/custom_backend/phi3.py
+++ specforge/modeling/target/custom_backend/phi3.py
@@ -354,13 +354,11 @@ class Phi3Model(Phi3PreTrainedModel):
                 position_embeddings=position_embeddings,
                 **kwargs,
             )
-
-            if output_hidden_states:
-                if (
-                    layers_to_output_hidden_states is None
-                    or idx in layers_to_output_hidden_states
-                ):
-                    all_hidden_states += (hidden_states,)
+            if (
+                layers_to_output_hidden_states is None
+                or idx in layers_to_output_hidden_states
+            ):
+                all_hidden_states += (hidden_states,)
 
         hidden_states = self.norm(hidden_states)
 
